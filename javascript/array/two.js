a3c2fb67fa8dbfda7e66af6e59e0d8f2d808e595
chaitanya <chaitanya3052000@gmail.com>
2022-07-20 11:18:11 +0530

    hey

--- javascript/array/two.js
+++ javascript/array/two.js
@@ -1,3 +1,11 @@
-let a=[hja,jshj,sbj,sjk,jsjk]
+let a=[1,2,"hello","hi",'have lunch',true,false,1,{},[],3333.3]
 console.log(a[1])
 console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+console.log(a[5])
+console.log(a[6])
+for( let i=0;i<=9;i++)
+{
+console.log(a[i])
+}
