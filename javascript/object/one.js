532f496c697476c3ff2b6f7536525d3f4f77a277
chaitanya <chaitanya3052000@gmail.com>
2022-07-21 10:58:49 +0530

    hey

---
+++ javascript/object/one.js
@@ -0,0 +1,8 @@
+let salary = 35000
+if (salary >40000)
+{
+    console.log("yes,eligible for marriage")
+}
+else{
+    console.log("no,please wait")
+}
---
+++ javascript/object/two.js
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <body>
+            <p id='abc'></p>
+            <script>
+                let tag_ref =document.getelementbyid('abc')
+                tag_ref.innerhtml "hello,rahul gandhi"            </script>
+        </body>
+    </head>
+</html>
