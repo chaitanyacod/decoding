7dc6ec9d290e4e2ced3a0ccb9cfe10e0a06b27f0
chaitanyacod <chaitanya3052000@gmail.com>
2022-09-06 13:17:22 +0530

    hey

--- javascript/Assignments/one.js
+++ javascript/Assignments/one.js
@@ -1,15 +1,6 @@
-
-<html>
-<head>
-   <script>
-    var a=1000
-    if (a%7==0)
-    console.log("divisible with 7")
-    else
-    console.log("divisible without 7")
-   </script>
-</head>
-<body>
-    
-</body>
-</html>
+variable =
+if (variable % 7 == 0) {
+  console.log("divisible")
+} else {
+ console.log("not divisible") 
+}
---
+++ javascript/operations/one.js
@@ -0,0 +1,6 @@
+function add(a,b)
+{
+return a+b+50;
+}
+let result=add(100,50)
+console.log(result);
